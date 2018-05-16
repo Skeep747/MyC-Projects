f650ce9901bd88c8dfa793a61693ad3b2b6eec24
Alexander Sadomov <Skeep747@gmail.com>
2018-05-16 23:46:19 +0300

    Add files via upload

---
+++ FastClickGame/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace FastClickGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lb1 = new System.Windows.Forms.Label();
+            this.bt1 = new System.Windows.Forms.Button();
+            this.lb2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lb1
+            // 
+            this.lb1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lb1.Font = new System.Drawing.Font("Modern No. 20", 71.99999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb1.Location = new System.Drawing.Point(12, 65);
+            this.lb1.Name = "lb1";
+            this.lb1.Size = new System.Drawing.Size(134, 100);
+            this.lb1.TabIndex = 0;
+            this.lb1.Text = "10";
+            this.lb1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lb1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.lb1_MouseDown);
+            // 
+            // bt1
+            // 
+            this.bt1.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt1.Location = new System.Drawing.Point(12, 7);
+            this.bt1.Name = "bt1";
+            this.bt1.Size = new System.Drawing.Size(132, 38);
+            this.bt1.TabIndex = 1;
+            this.bt1.Text = "Start clicking";
+            this.bt1.UseVisualStyleBackColor = true;
+            this.bt1.Click += new System.EventHandler(this.bt1_Click);
+            // 
+            // lb2
+            // 
+            this.lb2.Font = new System.Drawing.Font("Modern No. 20", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb2.Location = new System.Drawing.Point(9, 182);
+            this.lb2.Name = "lb2";
+            this.lb2.Size = new System.Drawing.Size(135, 23);
+            this.lb2.TabIndex = 2;
+            this.lb2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(157, 204);
+            this.Controls.Add(this.lb2);
+            this.Controls.Add(this.bt1);
+            this.Controls.Add(this.lb1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Click through rate";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lb1;
+        private System.Windows.Forms.Button bt1;
+        private System.Windows.Forms.Label lb2;
+    }
+}
+
---
+++ FastClickGame/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace FastClickGame
+{
+    public partial class Form1 : Form
+    {
+        long time_start;
+        long time_end;
+        int max_counter = 10, counter;
+        string mode = "stop";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void bt1_Click(object sender, EventArgs e)
+        {
+            StartGame();
+        }
+
+        private void PlayGame()
+        {
+            if (mode == "stop") return;
+            if (counter == max_counter) time_start = DateTime.Now.Ticks;
+            if (counter > 0) counter--;
+            lb1.Text = counter.ToString();
+            if (counter == 0) StopGame();
+        }
+
+        private void StopGame()
+        {
+            mode = "stop";
+            time_end = DateTime.Now.Ticks;
+            double seconds = TimeSpan.FromTicks(time_end - time_start).TotalSeconds;
+            double click_per_seconds = max_counter / seconds;
+            lb2.Text = click_per_seconds.ToString("#.#") + " clicks per second";
+            bt1.Enabled = true;
+        }
+
+        private void lb1_MouseDown(object sender, MouseEventArgs e)
+        {
+            PlayGame();
+        }
+
+        private void StartGame()
+        {
+           if (mode == "play") return;
+            mode = "play";
+            counter = max_counter;
+            lb1.Text = counter.ToString();
+            lb2.Text = "";
+            bt1.Enabled = false;
+        }
+    }
+}
