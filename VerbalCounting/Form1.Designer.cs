08284ec1c51e0844b9586eada54d879cb14f5a94
Alexander Sadomov <Skeep747@gmail.com>
2018-05-16 23:56:14 +0300

    Add files via upload

---
+++ VerbalCounting/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace Verbal_counting
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textAnswer = new System.Windows.Forms.TextBox();
+            this.labelThink = new System.Windows.Forms.Label();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.progress = new System.Windows.Forms.ProgressBar();
+            this.buttonSub = new System.Windows.Forms.Button();
+            this.buttonMult = new System.Windows.Forms.Button();
+            this.buttonDev = new System.Windows.Forms.Button();
+            this.textBoxLevel = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textAnswer
+            // 
+            this.textAnswer.Font = new System.Drawing.Font("Verdana", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textAnswer.Location = new System.Drawing.Point(22, 130);
+            this.textAnswer.Name = "textAnswer";
+            this.textAnswer.Size = new System.Drawing.Size(331, 85);
+            this.textAnswer.TabIndex = 0;
+            this.textAnswer.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textAnswer.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textAnswer_KeyDown);
+            // 
+            // labelThink
+            // 
+            this.labelThink.BackColor = System.Drawing.Color.White;
+            this.labelThink.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.labelThink.Font = new System.Drawing.Font("Verdana", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelThink.Location = new System.Drawing.Point(22, 19);
+            this.labelThink.Name = "labelThink";
+            this.labelThink.Size = new System.Drawing.Size(331, 94);
+            this.labelThink.TabIndex = 1;
+            this.labelThink.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonAdd.Location = new System.Drawing.Point(22, 245);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(63, 49);
+            this.buttonAdd.TabIndex = 2;
+            this.buttonAdd.Tag = "add";
+            this.buttonAdd.Text = "Add";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // progress
+            // 
+            this.progress.Location = new System.Drawing.Point(22, 245);
+            this.progress.Name = "progress";
+            this.progress.Size = new System.Drawing.Size(331, 49);
+            this.progress.TabIndex = 3;
+            this.progress.Visible = false;
+            // 
+            // buttonSub
+            // 
+            this.buttonSub.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonSub.Location = new System.Drawing.Point(91, 245);
+            this.buttonSub.Name = "buttonSub";
+            this.buttonSub.Size = new System.Drawing.Size(63, 49);
+            this.buttonSub.TabIndex = 4;
+            this.buttonSub.Tag = "sub";
+            this.buttonSub.Text = "Sub";
+            this.buttonSub.UseVisualStyleBackColor = true;
+            this.buttonSub.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // buttonMult
+            // 
+            this.buttonMult.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonMult.Location = new System.Drawing.Point(160, 245);
+            this.buttonMult.Name = "buttonMult";
+            this.buttonMult.Size = new System.Drawing.Size(63, 49);
+            this.buttonMult.TabIndex = 5;
+            this.buttonMult.Tag = "mult";
+            this.buttonMult.Text = "Mult";
+            this.buttonMult.UseVisualStyleBackColor = true;
+            this.buttonMult.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // buttonDev
+            // 
+            this.buttonDev.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonDev.Location = new System.Drawing.Point(229, 245);
+            this.buttonDev.Name = "buttonDev";
+            this.buttonDev.Size = new System.Drawing.Size(63, 49);
+            this.buttonDev.TabIndex = 6;
+            this.buttonDev.Tag = "dev";
+            this.buttonDev.Text = "Dev";
+            this.buttonDev.UseVisualStyleBackColor = true;
+            this.buttonDev.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // textBoxLevel
+            // 
+            this.textBoxLevel.Font = new System.Drawing.Font("Verdana", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxLevel.Location = new System.Drawing.Point(298, 246);
+            this.textBoxLevel.MaxLength = 2;
+            this.textBoxLevel.Name = "textBoxLevel";
+            this.textBoxLevel.Size = new System.Drawing.Size(55, 46);
+            this.textBoxLevel.TabIndex = 7;
+            this.textBoxLevel.Text = "10";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(365, 306);
+            this.Controls.Add(this.textBoxLevel);
+            this.Controls.Add(this.buttonDev);
+            this.Controls.Add(this.buttonMult);
+            this.Controls.Add(this.buttonSub);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.labelThink);
+            this.Controls.Add(this.textAnswer);
+            this.Controls.Add(this.progress);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Verbal counting";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textAnswer;
+        private System.Windows.Forms.Label labelThink;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.ProgressBar progress;
+        private System.Windows.Forms.Button buttonSub;
+        private System.Windows.Forms.Button buttonMult;
+        private System.Windows.Forms.Button buttonDev;
+        private System.Windows.Forms.TextBox textBoxLevel;
+    }
+}
+
---
+++ VerbalCounting/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Verbal_counting
+{
+    public partial class Form1 : Form
+    {
+        int count, countMax = 10;
+        int min = 1, max = 10;
+        Random rand = new Random();
+        string answer;
+        string mode;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            Start(((Button)sender).Tag.ToString());
+        }
+
+        private void textAnswer_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                if (textAnswer.Text == answer)
+                {
+                    progress.Value = count;
+                    if (count == countMax)
+                        GameWin();
+                    else
+                    {
+                        textAnswer.Text = "";
+                        Think();
+                    }
+                }
+                else
+                    Gameover();
+            }
+        }
+        private void Start(string _mode)
+        {
+            mode = _mode;
+            textAnswer.Text = "";
+            buttonAdd.Visible = false;
+            buttonSub.Visible = false;
+            buttonMult.Visible = false;
+            buttonDev.Visible = false;
+            textBoxLevel.Visible = false;
+            progress.Visible = true;
+            progress.Minimum = 0;
+            progress.Maximum = countMax;
+            progress.Value = 0;
+            count = 0;
+            try
+            {
+                max = Convert.ToInt32(textBoxLevel.Text);
+            }
+            catch
+            {
+                max = 10;
+                textBoxLevel.Text = max.ToString();
+            }
+            Think();
+        }
+
+        private void Think()
+        {
+            int a, b;
+            a = rand.Next(min, max + 1);
+            b = rand.Next(min, max + 1);
+            if (mode == "add")
+            {
+                labelThink.Text = a.ToString() + " + " + b.ToString() + " = ";
+                answer = (a + b).ToString();
+            }
+            if (mode == "sub")
+            {
+                if (a < b)
+                {
+                    int x = a;
+                    a = b;
+                    b = x;
+                }
+                labelThink.Text = a.ToString() + " – " + b.ToString() + " = ";
+                answer = (a - b).ToString();
+            }
+            if (mode == "mult")
+            {
+                labelThink.Text = a.ToString() + " * " + b.ToString() + " = ";
+                answer = (a * b).ToString();
+            }
+            if (mode == "dev")
+            {
+                labelThink.Text = (a*b).ToString() + " / " + b.ToString() + " = ";
+                answer = a.ToString();
+            }
+            count++;
+        }
+
+        private void Gameover()
+        {
+            textAnswer.Text = answer;
+            progress.Visible = false;
+            buttonAdd.Visible = true;
+            buttonSub.Visible = true;
+            buttonMult.Visible = true;
+            buttonDev.Visible = true;
+            textBoxLevel.Visible = true;
+        }
+
+        private void GameWin()
+        {
+            MessageBox.Show("You win!", "Congratulations!");
+            progress.Visible = false;
+            buttonAdd.Visible = true;
+            buttonSub.Visible = true;
+            buttonMult.Visible = true;
+            buttonDev.Visible = true;
+            textBoxLevel.Visible = true;
+        }
+    }
+}
