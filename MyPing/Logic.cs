58836808dd7335b784a4629dd8d42b093f08458d
Alexander Sadomov <Skeep747@gmail.com>
2018-05-16 23:56:58 +0300

    Add files via upload

---
+++ MyPing/Logic.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net.NetworkInformation;
+using System.Text;
+
+namespace WindowsFormsApp2
+{
+    class Logic
+    {
+        public string Start_ping(string ip)
+        {
+            Ping pingSender = new Ping();
+            PingOptions options = new PingOptions();
+            string data = "Hello google!";
+            byte[] buffer = Encoding.ASCII.GetBytes(data);
+            int timeout = 120;
+            PingReply reply = pingSender.Send(ip, timeout, buffer, options);
+            if (reply.Status == IPStatus.Success)
+                return reply.Address.ToString() + " OK! Roundtrip time " + 
+                       reply.RoundtripTime.ToString() + " ms. " + 
+                       reply.Options.Ttl.ToString() + " Ttl :)";
+            else return "Destination host unreachable :(";
+        }
+    }
+}
