5ee02fd6afa122ba051ad7a72b7d54f4caf405dd
Alexander Sadomov <Skeep747@gmail.com>
2018-05-20 13:16:07 +0300

    Add files via upload

---
+++ Reaction/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace Reaction
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Image = global::Reaction.Properties.Resources._1;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 34);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(150, 150);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox2.Image = global::Reaction.Properties.Resources._2;
+            this.pictureBox2.Location = new System.Drawing.Point(168, 34);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(150, 150);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox3.Image = global::Reaction.Properties.Resources._3;
+            this.pictureBox3.Location = new System.Drawing.Point(324, 34);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(150, 150);
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonStart.Location = new System.Drawing.Point(168, 191);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(150, 40);
+            this.buttonStart.TabIndex = 3;
+            this.buttonStart.Text = "Start Game";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Yu Gothic UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(460, 23);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Text :)";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(12, 237);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(460, 14);
+            this.progressBar.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 255);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Reaction speed";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar progressBar;
+    }
+}
+
---
+++ Reaction/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace Reaction
+{
+    public partial class Form1 : Form
+    {
+        int totalClicks = 10;
+        int clickNr = -1;
+        int reactionTime = 0;
+        int myCardNr = 0;
+        int waiting = 0;
+        int waitingMin = 1;
+        int waitingMax = 3;
+
+        Stopwatch watch = new Stopwatch();
+        Random rand = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+            progressBar.Maximum = totalClicks;
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            clickNr = 0;
+            reactionTime = 0;
+            buttonStart.Visible = false;
+            ShowCard(0);
+            NextClick();
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            if (clickNr < 0)
+            {
+                return;
+            }
+
+            if (waiting > 0)
+            {
+                waiting--;
+                if (waiting == 0)
+                {
+                    ShowCard(rand.Next(1, 4));
+                    watch.Restart();
+                }
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            watch.Stop();
+            reactionTime += (int)watch.ElapsedMilliseconds;
+            progressBar.Value = clickNr;
+            if (clickNr >= totalClicks)
+            {
+                ShowResults();
+            }
+            else
+            {
+                NextClick();
+            }
+        }
+
+        void ShowCard(int nr)
+        {
+            pictureBox1.Visible = nr == 1;
+            pictureBox2.Visible = nr == 2;
+            pictureBox3.Visible = nr == 3;
+        }
+
+        private void NextClick()
+        {
+            ShowCard(0);
+            clickNr++;
+            waiting = rand.Next(waitingMin * 1000 / timer.Interval, waitingMax * 1000 / timer.Interval + 1);
+        }
+
+        private void ShowResults()
+        {
+            double sec = reactionTime / 1000.0 / totalClicks;
+            MessageBox.Show("Reaction time: " + sec.ToString("0.000") + " seconds");
+            buttonStart.Visible = true;
+            clickNr = -1;
+        }
+    }
+}
